b718d26128b5931484b7c96a8bd57328357c8a74
makifgulmus <makifgulmus@gmail.com>
2023-05-09 18:22:55 +0300

    add fastest runner calculation program

---
+++ IstanbulRunfest.java
@@ -0,0 +1,49 @@
+public class IstanbulRunfest {
+    public static class Runner {
+        public String runnerName;
+        public int runnerTime;
+
+        public Runner(String name, int time) {
+            this.runnerName = name;
+            this.runnerTime = time;
+        }
+    }
+
+    public static Runner calculateFastestRunner(Runner[] runners) {
+        Runner fastestRunner = runners[0];
+        for (int i = 1; i < runners.length; i++) {
+            if (runners[i].runnerTime < fastestRunner.runnerTime) {
+                fastestRunner = runners[i];
+            }
+        }
+        return fastestRunner;
+    }
+
+    public static void main(String[] args) {
+        Runner[] runners = {
+            new Runner("Ibrahim", 272),
+            new Runner("Berke", 479),
+            new Runner("Metin", 278),
+            new Runner("Irem", 329),
+            new Runner("Yigit", 445),
+            new Runner("Melis", 402),
+            new Runner("Mehmet", 388),
+            new Runner("Akif", 275),
+            new Runner("Furkan", 243),
+            new Runner("Fırat", 334),
+            new Runner("Tolga", 412),
+            new Runner("Ozkan", 393),
+            new Runner("Umut", 299),
+            new Runner("Seda", 343),
+            new Runner("Selcan", 317),
+            new Runner("Hatice", 265)
+        };
+        Runner fastestRunner = calculateFastestRunner(runners);
+        System.out.println("The fastest runner is " + fastestRunner.runnerName + 
+        " with a time of " + fastestRunner.runnerTime + " seconds.");
+    }
+
+    
+
+   
+}
